2d57b6ffdd27ef964ac2ede7c007d96cbbe957ac
Hridoy Suttradhar <hridoysuttradhar1123@gmail.com>
2025-06-16 02:48:40 +0600

    A placeholder tells the printf function to prepare itself to receive a int,char.string stc

---
+++ week-1/01-hello4.c
@@ -0,0 +1,10 @@
+// get_string and printf with %s
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string answer = get_string("What's your name? ");
+    printf("Hello, %s\n",answer);
+}
