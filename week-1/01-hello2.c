d9da4e4c54cb9ea0f0115cfa3a895fdb9a222573
Hridoy Suttradhar <hridoysuttradhar1123@gmail.com>
2025-06-16 02:45:29 +0600

    Introduced escape characters like '\n\, '\\' etc

---
+++ week-1/01-hello2.c
@@ -0,0 +1,16 @@
+// Restore the program
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, world!\n");
+}
+
+//This '\' character is called an escape character that tells the compiler
+// that \n is special instruction to create a new line break
+// \n create a new line 
+// \r return to the start of a line
+// \" print a double quote 
+// \' print a single quote
+// \\ print a backslash
