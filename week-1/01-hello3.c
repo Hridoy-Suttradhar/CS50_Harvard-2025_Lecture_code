259b945ed161723460a77438e46d1e2a2e543917
Hridoy Suttradhar <hridoysuttradhar1123@gmail.com>
2025-06-16 02:46:59 +0600

    Tried using string, But forgot to add essential library

---
+++ week-1/01-hello3.c
@@ -0,0 +1,9 @@
+// get_string and printf with incorrect placeholder
+
+#include<stdio.h>
+
+int main(void)
+{
+    // strings answer = get_string("What's your name? ");
+    //printf("hello, answer\n")
+}
