1c613c788655e1d1845a57becc1ed46e58a35590
Hridoy Suttradhar <hridoysuttradhar1123@gmail.com>
2025-06-17 08:57:04 +0600

    long

---
+++ week-1/06-calculator2.c
@@ -0,0 +1,22 @@
+//long 
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    long dollars = 1;
+    while(true)
+    {
+        char c = get_char("Here's $%li. Double it and give to next person? ",dollars);
+        if (c == 'y')
+        {
+            dollars *= 2;
+        }
+        else 
+        {
+            break;
+        }
+    }
+    printf("Here's $%li.\n", dollars);
+}
