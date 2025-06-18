116adb5bb749d929858313c9c92b8ca2643ff803
Hridoy Suttradhar <hridoysuttradhar1123@gmail.com>
2025-06-19 04:08:07 +0600

    Average three (hardcoded) numbers without array

---
+++ week-2/01-scores0.c
@@ -0,0 +1,14 @@
+// Average three (hardcoded) numbers
+
+#include <stdio.h>
+
+int main(void)
+{
+    // Scores
+    int score1 = 73;
+    int score2 = 72;
+    int score3 = 33;
+
+    // Print average
+    printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+}
